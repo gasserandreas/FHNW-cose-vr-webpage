5efa630c0244988a716113702c3d437adc9e4407
Andreas Gasser <andreas.safe@gmail.com>
2017-06-08 23:36:58 +0200

    added latest db scripts

--- Database/developer_queries.sql
+++ Database/developer_queries.sql
@@ -23,6 +23,22 @@ LEFT JOIN TagToDevice tagToDevice
 	ON tagToDevice.TagId = tag.id
 LEFT JOIN Device device
 	ON device.id = tagToDevice.DeviceId
-WHERE device.name IS NOT NULL
+WHERE device.name IS NOT NULL AND items.Latitude IS NOT NULL
 ;
 
+SELECT DISTINCT
+    items.*,
+    device.id as DeviceId,
+    device.name as DeviceName
+FROM SearchItems items
+LEFT JOIN TagToItem tagToItem
+    ON items.ItemId = tagToItem.ItemId
+LEFT JOIN Tag tag
+    ON tagToItem.TagId = tag.id
+LEFT JOIN TagToDevice tagToDevice
+    ON tagToDevice.TagId = tag.id
+LEFT JOIN Device device
+    ON device.id = tagToDevice.DeviceId
+WHERE device.name IS NOT NULL AND items.ItemId = '112244278859';
+
+-- location
