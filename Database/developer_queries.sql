86304a9dd8730c24b18ee898bb7a55cfdc2bb034
Andreas Gasser <andreas.safe@gmail.com>
2017-06-10 17:42:50 +0200

    latest db queries

--- Database/developer_queries.sql
+++ Database/developer_queries.sql
@@ -1,5 +1,17 @@
 SELECT * FROM SearchItems;
 
+SELECT Country, COUNT(*) as countItem From SearchItems
+GROUP BY Country
+ORDER BY countItem DESC;
+
+select * from SearchItems where Country = 'IT';
+
+select COUNT(*) AS counter, Longitude, Latitude, country FROM SearchItems
+WHERE Longitude IS NOT NULL
+GROUP BY Longitude, Latitude, country
+ORDER BY counter DESC;
+;
+
 SELECT COUNT(*) FROM Tag;
 
 SELECT COUNT(*) FROM TagToItem;
@@ -10,6 +22,36 @@ SELECT * FROM Device;
 
 SELECT * FROM SearchItems;
 
+SELECT * FROM Tag
+WHERE 
+	value LIKE 'microsoft'
+    ;
+/*
+( 13, 5)
+( 14, 5)
+( 39, 3)
+( 42, 1)
+( 69, 4)
+( 80, 6)
+( 87, 1)
+( 124, 2)
+( 132, 3)
+( 133, 2)
+( 134, 4)
+( 834, 6)
+( 676, 7)
+( ,)
+/*
+(1,'Sony PlayStation VR'),
+(2,'HTC Vive'),
+(3,'Oculus Rift'),
+(4,'Google Daydream View'),
+(5,'Samsung Gear VR'),
+(6,'Merge VR Goggles'),
+(7,'Hololense')
+*/
+
+select count(*), search.DeviceName FROM (
 SELECT DISTINCT
     items.*,
     device.id as DeviceId,
@@ -24,6 +66,8 @@ LEFT JOIN TagToDevice tagToDevice
 LEFT JOIN Device device
 	ON device.id = tagToDevice.DeviceId
 WHERE device.name IS NOT NULL AND items.Latitude IS NOT NULL
+) search
+GROUP BY search.DeviceId
 ;
 
 SELECT DISTINCT
